722cd323d70d8b0438a8f33a8a670e3cc05bad09
Mayank Sharma <46962063+GeekyMayank19@users.noreply.github.com>
2021-05-22 22:28:34 +0530

    Revert "frontend started"
    This reverts commit c62d63a2e2af275f2a1a5dcec912ad80f7dd06db.

--- updatedUhub/frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Router from './Router';
-
-ReactDOM.render(
-  <Router/>,
-  document.getElementById('root')
-);
