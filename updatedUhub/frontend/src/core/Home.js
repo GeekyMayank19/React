c62d63a2e2af275f2a1a5dcec912ad80f7dd06db
Mayank Sharma <46962063+GeekyMayank19@users.noreply.github.com>
2021-05-22 22:07:12 +0530

    frontend started

---
+++ updatedUhub/frontend/src/core/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Base from './Base'
+
+const Home= () => {
+    return (
+        <div>
+        <Base></Base>
+            HOme
+        </div>
+    )
+}
+export default Home
